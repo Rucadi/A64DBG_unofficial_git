8522d0aa7229d68d5b99671007f3040e5fc5fc23
GeekNeo <971159199@qq.com>
2020-12-30 12:39:19 +0800

    add android debugee.

---
+++ debugee/android/jni/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := debugme
+LOCAL_SRC_FILES := ../../debugme.cpp 
+
+LOCAL_CFLAGS += -fvisibility=hidden
+LOCAL_CPPFLAGS += -std=gnu++17
+LOCAL_LDLIBS += -llog
+
+include $(BUILD_EXECUTABLE)
---
+++ debugee/android/jni/Application.mk
@@ -0,0 +1,4 @@
+APP_STL := c++_static
+NDK_TOOLCHAIN_VERSION := clang
+APP_ABI := arm64-v8a
+APP_PLATFORM := android-24
